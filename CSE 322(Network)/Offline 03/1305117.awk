34070a84b14865f6f3a371d38acd150296838c44
unknown <1305117.pc@ugrad.cse.buet.ac.bd>
2017-04-25 23:13:02 +0600

    Almost done Paramterization complete

---
+++ CSE 322(Network)/Offline 03/1305117.awk
@@ -0,0 +1,227 @@
+BEGIN {
+	max_node = 2000;
+	nSentPackets = 0.0 ;		
+	nReceivedPackets = 0.0 ;
+	rTotalDelay = 0.0 ;
+	max_pckt = 10000;
+
+	header = 20;	
+
+	idHighestPacket = 0;
+	idLowestPacket = 100000;
+	rStartTime = 10000.0;
+	rEndTime = 0.0;
+	nReceivedBytes = 0;
+	rEnergyEfficeincy = 0;
+
+	nDropPackets = 0.0;
+
+	total_energy_consumption = 0;
+
+	temp = 0;
+	
+	for (i=0; i<max_node; i++) {
+		energy_consumption[i] = 0;		
+	}
+
+	total_retransmit = 0;
+	for (i=0; i<max_pckt; i++) {
+		retransmit[i] = 0;		
+	}
+
+}
+{
+	#  ==========================================================
+	# Initialization which column  represents which metric
+	# ===========================================================
+	strEvent = $1 ;		                       # receive /send	
+	rTime = $2 ;                               # time
+	node = $3 ;                                # src node
+	strAgt = $4 ;		                       # mac	
+	idPacket = $6 ;                            # packet no
+	strType = $7 ;		                       # str type ack,message	
+	nBytes = $8;                               # bytes in packet
+	energy = $13;		                       # energy string
+	total_energy = $14;                        # total energy
+	idle_energy_consumption = $16;	           # idle energy
+	sleep_energy_consumption = $18;            # sleep energy
+	transmit_energy_consumption = $20;         # transmit energy
+	receive_energy_consumption = $22;          # received energy
+	num_retransmit = $30;                      # retransmit
+	
+	sub(/^_*/, "", node);
+	sub(/_*$/, "", node);
+	# =======================================================
+	# Energy
+	#========================================================
+	if (energy == "[energy") {
+		energy_consumption[node] = (idle_energy_consumption + sleep_energy_consumption + transmit_energy_consumption + receive_energy_consumption);
+	}
+
+	# =======================================================
+	# High low packet
+	#========================================================
+
+#	if (strType == "cbr") {
+		if (idPacket > idHighestPacket) idHighestPacket = idPacket;
+		if (idPacket < idLowestPacket) idLowestPacket = idPacket;
+#		}
+
+		
+	# =======================================================
+	# Start time
+	#========================================================	
+
+	if(rTime<rStartTime) {
+			rStartTime=rTime;
+		}
+
+	# =======================================================
+	# End time
+	#========================================================	
+	if(rTime>rEndTime) {
+		rEndTime=rTime;
+		}
+
+
+
+	# =======================================================
+	# Sent packets 
+	#========================================================
+#	if ( strEvent == "s" &&   strType == "cbr" ) {	
+	if ( strEvent == "s"  ) {
+			nSentPackets += 1 ;	
+			rSentTime[ idPacket ] = rTime ;
+		}
+
+
+
+	# =======================================================
+	# Packet delay calculation
+	#========================================================
+#		if ( strEvent == "r" ) {
+		if ( strEvent == "r" && idPacket >= idLowestPacket) {
+			nReceivedPackets += 1 ;	
+			if(  nBytes>0)	{
+					nReceivedBytes += (nBytes-header);
+			}
+			
+#			printf("bytes %d\n",nBytes);
+			rReceivedTime[ idPacket ] = rTime ;
+			rDelay[idPacket] = rReceivedTime[ idPacket] - rSentTime[ idPacket ];
+			rTotalDelay += rDelay[idPacket]; 
+
+		}
+	# =======================================================
+	# Drop packet calculation
+	#========================================================
+#	if( strEvent == "D"   &&   strType == "cbr" )
+	if( strEvent == "D" )
+	{
+		nDropPackets += 1;
+	}
+
+
+	# =======================================================
+	# Retransmission of packet
+	#========================================================
+	if( strType == "tcp" )
+	{
+		retransmit[idPacket] = num_retransmit;		
+	}
+
+	
+}
+END {
+
+	# =======================================================
+	# Ultimate Receive time (total work time)
+	#========================================================
+	rTime = rEndTime - rStartTime ;
+
+
+	# =======================================================
+	# Throughput calculation
+	#========================================================
+	if(rTime!=0)
+	{
+		rThroughput = nReceivedBytes*8 / rTime;
+	}
+	
+
+	
+
+	# =======================================================
+	# Packet delivery ratio
+	#========================================================
+	if(nSentPackets!=0)
+	{
+		rPacketDeliveryRatio = (nReceivedPackets / nSentPackets) * 100 ;
+	}
+	
+
+
+	# =======================================================
+	# Packet drop ratio
+	#========================================================
+
+	if(nSentPackets!=0)
+	{
+		rPacketDropRatio = (nDropPackets / nSentPackets) * 100;
+	}
+
+
+
+	# =======================================================
+	# Total energy consumption
+	#========================================================
+	for(i=0; i<max_node;i++) {
+		total_energy_consumption += energy_consumption[i];
+	}
+
+
+	# =======================================================
+	# Average delay and energy per packet calculation
+	#========================================================
+	if ( nReceivedPackets != 0 ) {
+		rAverageDelay = rTotalDelay / nReceivedPackets ;
+		avg_energy_per_packet = total_energy_consumption / nReceivedPackets ;
+	}
+
+
+
+	# ==================================================================
+	# Average energy per byte,per bit and energy efficeincy Calculation
+	#===================================================================
+	if ( nReceivedBytes != 0 ) {
+		avg_energy_per_byte = total_energy_consumption / nReceivedBytes ;
+		avg_energy_per_bit = avg_energy_per_byte / 8;
+		rEnergyEfficeincy = total_energy_consumption / (nReceivedBytes*8);
+	}
+
+
+	# =======================================================
+	# Total retransmission
+	#================================printf("Packet drop ratio : %f\n",rPacketDropRatio);========================
+	for (i=0; i<max_pckt; i++) {
+		total_retransmit += retransmit[i] ;		
+	}
+	printf("Total drop : %d\n",nDropPackets);
+	printf("Total sent : %d\n",nSentPackets);
+	printf("Total received : %d\n",nReceivedPackets);
+	printf("Total time : %d\n",rTime);
+	printf("Throughput : %lf\n",rThroughput);
+	printf("Packet delivery ratio : %f\n",rPacketDeliveryRatio);
+	printf("Packet drop ratio : %f\n",rPacketDropRatio);
+	printf("Total energy consumption : %f\n",total_energy_consumption);
+	printf("Average delay : %f\n",rAverageDelay);
+	printf("Average energy per byte : %f\n",avg_energy_per_byte);
+	printf("Average energy per bit : %f\n",avg_energy_per_bit);
+	printf("energy Efficiency : %f\n",rEnergyEfficeincy);
+	printf("Total retransmit : %d\n",total_retransmit);
+
+
+
+#	printf("3 %lf\n",rThroughput);
+	
+}
--- CSE 322(Network)/Offline 03/1305117_802_11.tcl
+++ CSE 322(Network)/Offline 03/1305117_802_11.tcl
@@ -1,45 +1,57 @@
-# ============================================================================
+# =======================================================================
 # Network set up
-# ============================================================================
+# =======================================================================
 set val(chan)           Channel/WirelessChannel    ;#Channel Type
 set val(prop)           Propagation/TwoRayGround   ;# radio-propagation model
-set val(netif)          Phy/WirelessPhy/802_15_4            ;# network interface type
-set val(mac)            Mac/802_15_4                 ;# MAC type
-set val(ifq)            CMUPriQueue             ;# interface queue type Queue/DropTail/PriQueue CMUPriQueue
+set val(netif)          Phy/WirelessPhy            ;# network interface type
+set val(mac)            Mac/802_11                 ;# MAC type
+set val(ifq)            CMUPriQueue             ;# interface queue type Queue/DropTail/PriQueue 
 set val(ll)             LL                         ;# link layer type
 set val(ant)            Antenna/OmniAntenna        ;# antenna model
 set val(ifqlen)         50                        ;# max packet in ifq                        ;# number of mobilenodes
-set val(rp)             DSDV               ;# routing protocol
+set val(rp)             DSR               ;# routing protocol
 set val(x)              600
 set val(y)              600
 set val(row)             0
 set val(col)             0
-set val(nn)              20
-set val(flow)            30
+set val(nn)              [lindex $argv 0]
+set val(flow)            [lindex $argv 1]
 set grid                 1
 set val(ttime)           100
-set val(extraTime)       10
-set val(velocity)         5
-
-
+set val(extraTime)       1
+set val(range)           [lindex $argv 2]
+# ==========================================================================
+# Distance
+# ==========================================================================
+set range(1Tx)       3.65262e-10
+set range(2Tx)       2.28289e-11
+set range(3Tx)       4.50941e-12
+set range(4Tx)       1.42681e-12
+set range(5Tx)       5.8442e-13
+# ==========================================================================
+# Distance Settings
+# ==========================================================================
+Phy/WirelessPhy set CPThresh_ 10.0
+Phy/WirelessPhy set CSThresh_ $range($val(range)Tx)
+Phy/WirelessPhy set RXThresh_ $range($val(range)Tx)
 # ==========================================================================
 # Mac settings
 # =========================================================================
-Mac/802_15_4 set dataRate_ 11Mb
-Mac/802_15_4 set syncFlag_ 1
-Mac/802_15_4 set dutyCycle_ cbr_interval
-# ===========================================================================
+Mac/802_11 set dataRate_ 11Mb
+Mac/802_11 set syncFlag_ 1
+Mac/802_11 set dutyCycle_ cbr_interval
+# =============================================================================
 # Energy parameter
-# ===========================================================================
+# ==============================================================================
 set val(energymodel_11)    EnergyModel     ;
 set val(initialenergy_11)  1000            ;# Initial energy in Joules
 
-set val(idlepower_11) 869.4e-3      ;#LEAP (802.11g) 
-set val(rxpower_11) 1560.6e-3     ;#LEAP (802.11g)
-set val(txpower_11) 1679.4e-3     ;#LEAP (802.11g)
-set val(sleeppower_11) 37.8e-3      ;#LEAP (802.11g)
-set val(transitionpower_11) 176.695e-3    ;#LEAP (802.11g)  
-set val(transitiontime_11) 2.36     ;#LEAP (802.11g)
+set val(idlepower_11) 869.4e-3			;#LEAP (802.11g) 
+set val(rxpower_11) 1560.6e-3			;#LEAP (802.11g)
+set val(txpower_11) 1679.4e-3			;#LEAP (802.11g)
+set val(sleeppower_11) 37.8e-3			;#LEAP (802.11g)
+set val(transitionpower_11) 176.695e-3		;#LEAP (802.11g)	
+set val(transitiontime_11) 2.36			;#LEAP (802.11g)
 # =============================================================================
 # Calculating row column
 # =============================================================================
@@ -55,14 +67,18 @@ set val(transitiontime_11) 2.36     ;#LEAP (802.11g)
                     set val(col) [expr $val(nn)/$i]
                   }  
             }
+
+
+
 # =============================================================================
 #  Global Variables
 # =============================================================================
 set ns_         [new Simulator]
-set tracefd     [open 1305117_802_15.tr w]
+set tracefd     [open 1305117_802_11.tr w]
+#$ns_ use-newtrace
 $ns_ trace-all $tracefd
 
-set namtrace [open 1305117_802_15.nam w]
+set namtrace [open 1305117_802_11.nam w]
 $ns_ namtrace-all-wireless $namtrace $val(x) $val(y)
 
 # set up topography object
@@ -74,9 +90,14 @@ create-god $val(nn)
 
 # Create channel
 set chan_1_ [new $val(chan)]
-# ===============================================================
-# Node set up
-# ===============================================================
+
+
+
+
+
+
+
+
 $ns_ node-config -adhocRouting $val(rp) \
                 -llType $val(ll) \
                 -macType $val(mac) \
@@ -86,41 +107,38 @@ $ns_ node-config -adhocRouting $val(rp) \
                 -propType $val(prop) \
                 -phyType $val(netif) \
                 -topoInstance $topo \
-                -agentTrace ON \
+                -agentTrace OFF \
                 -routerTrace OFF \
                 -macTrace ON \
                 -movementTrace ON \
-                -channel $chan_1_  \
+                -channel $chan_1_ \
                 -energyModel $val(energymodel_11) \
-                -idlePower $val(idlepower_11) \
-                -rxPower $val(rxpower_11) \
-                -txPower $val(txpower_11) \
-                -sleepPower $val(sleeppower_11) \
-                -transitionPower $val(transitionpower_11) \
-                -transitionTime $val(transitiontime_11) \
-                -initialEnergy $val(initialenergy_11)
+			          -idlePower $val(idlepower_11) \
+			          -rxPower $val(rxpower_11) \
+			          -txPower $val(txpower_11) \
+          		  -sleepPower $val(sleeppower_11) \
+          		  -transitionPower $val(transitionpower_11) \
+			          -transitionTime $val(transitiontime_11) \
+			          -initialEnergy $val(initialenergy_11)
+
 
       for {set i 0} {$i < [expr $val(nn)]} {incr i} {
                   set node_($i) [$ns_ node]
-                   $node_($i) random-motion 0
-                   $node_($i) start
+                  $node_($i) random-motion 0  
                   #puts "creating node $i"            ;
                # set mac_($i) [$node_($i) getMac 0]
                 # $mac_($i) set RTSThreshold_ 3000       
             }
 
 
-# =================================================================
-# Calculating position of node
-# =================================================================
+
+
 set x_start [expr $val(x)/($val(col)*2)];
 set y_start [expr $val(y)/($val(row)*2)];
 for {set i 0} {$i < $val(row) } { incr i } {
-#in same column
     for {set j 0} {$j < $val(col) } { incr j } {
     set m [expr $i*$val(col)+$j];
-
-  if {$grid == 1} {
+    if {$grid == 1} {
     set x_pos [expr $x_start+$j*($val(x)/$val(col))];#grid settings
     set y_pos [expr $y_start+$i*($val(y)/$val(row))];#grid settings
   } else {
@@ -130,20 +148,19 @@ for {set i 0} {$i < $val(row) } { incr i } {
   $node_($m) set X_ $x_pos;
   $node_($m) set Y_ $y_pos;
   $node_($m) set Z_ 0.0
+#  puts "now m $m"
   
+# puts "$m"
+#  puts  "$m x: [$node_($m) set X_] y: [$node_($m) set Y_] \n"
     }
     
 };
 
 
 
-
-
-
-
 for {set i 0} {$i < $val(nn)} {incr i} {
   $ns_ at 0.0 "$node_($i) label AP$i"
-  puts "Labeling node $i"            ;
+#  puts "Labeling node $i"            ;
 }
 
 for {set i 0} {$i < $val(nn) } { incr i } {
@@ -159,11 +176,8 @@ for {set i 0} {$i < $val(nn) } { incr i } {
     }
 }
 
-$ns_ at 0.0 "$node_([expr $val(nn)-1]) label Sink"
-
 Application/Traffic/CBR set packetSize_ 1023
 Application/Traffic/CBR set rate_ 256Kb
-
 # ========================================================
 # Number of sink calculating
 # ========================================================
@@ -210,78 +224,53 @@ for {set i 0} {$i < [expr $val(flow)]} {incr i} {
     puts "connecting $nodeStart with $con"
     incr nodeStart ;
 }
+
 # =====================================================================
 # Setting node size
 # =======================================================================
-
-
-
-
-
-
 for {set i 0} {$i < $val(nn)  } { incr i} {
   $ns_ initial_node_pos $node_($i) 100
 }
-# ===============================================================
-# Setting Random movement
-# ================================================================
-for {set i 5} {$i < [expr $val(ttime)-10]} {} {
-        set temp $val(nn)
-        $ns_ at  $i "random_movement $temp $val(x) $val(y) $val(velocity)"
-        set i [expr $i+30]
-      
-  } 
-
-proc random_movement { length x y v} {
-        global ns_ tracefd node_
-          set temp_x [expr int ($x*0.8)]
-          set temp_y [expr int ($y*0.8)]
-          puts "x $temp_x"
-          puts "y $temp_y"
-        for {set i 0} {$i < [expr $length]} {incr i} {
-
-          set x_pos [expr int($temp_x*rand())] ;#random settings
-          set y_pos [expr int($temp_y*rand())] ;#random settings
-
-          $node_($i) setdest $x_pos $y_pos $v
-      
-  }
-    
-    
-}
 
 
 
 
-for {set i 0} {$i < [expr $val(nn)-$nSink]} {incr i} {
+
+
+
+for {set i 0} {$i < [expr $val(nn) -$nSink]} {incr i} {
     #set time [expr $i % 4]
     $ns_ at 5.0 "$cbr1($i) start"
 }
 
 
+ 
 
 for {set i 0} {$i < [expr $val(nn)-$nSink] } {incr i} {
 
-  $ns_ at [expr $val(ttime)] "$cbr1($i) stop"
+	$ns_ at [expr $val(ttime)] "$cbr1($i) stop"
+
 }
-
-
 $ns_ at [expr $val(ttime)] "puts \"Simulation end\""
+
 $ns_ at [expr $val(ttime)+$val(extraTime)+$val(extraTime)] "puts \"NS EXITING...\" ; $ns_ halt"
 $ns_ at [expr $val(ttime)+$val(extraTime)] "stop"
 
 
 
 
+    
+    
 
 
 
 
 proc stop {} {
+    puts "Executing stop"
     global ns_ tracefd
     $ns_ flush-trace
     close $tracefd
-    exec nam 1305117_802_15.nam &
+    exec nam 1305117_802_11.nam &
     exit 0
 }
 
